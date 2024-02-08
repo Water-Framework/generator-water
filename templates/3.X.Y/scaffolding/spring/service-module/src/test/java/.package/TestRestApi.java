7e2c6cffef635215b347210e5426a0598d6ab9d2
Aristide <aristide.cittadino@acsoftware.it>
2024-02-08 11:13:03 +0100

    adding spring application launch before karate tests

--- templates/3.X.Y/scaffolding/spring/service-module/src/test/java/.package/TestRestApi.java
+++ templates/3.X.Y/scaffolding/spring/service-module/src/test/java/.package/TestRestApi.java
@@ -2,15 +2,31 @@
 package <%-projectGroupId%>;
 
 import com.intuit.karate.junit5.Karate;
-import it.water.core.api.service.Service;
-import it.water.core.testing.utils.junit.WaterTestExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 public class <%- projectSuffixUpperCase %>RestApiTest {
+
+    private static ConfigurableApplicationContext context;
+
+    @BeforeAll
+    public static void beforeClass() {
+        // Avvia l'applicazione Spring Boot e ottieni il contesto dell'applicazione
+        context = SpringApplication.run(<%- projectSuffixUpperCase %>Application.class);
+    }
     
     @Karate.Test
     Karate restInterfaceTest() {
         return Karate.run("classpath:karate");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        // Arresta l'applicazione Spring Boot
+        if (context != null) {
+            context.close();
+        }
     }
 }
