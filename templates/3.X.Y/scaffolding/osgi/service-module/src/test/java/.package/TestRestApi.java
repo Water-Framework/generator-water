f572c3b3653d9b2727ce3c238e7d0cb2e286dd2e
Aristide <aristide.cittadino@acsoftware.it>
2024-04-29 14:35:45 +0200

    adding await service for karaf tests and minor fixes

--- templates/3.X.Y/scaffolding/osgi/service-module/src/test/java/.package/TestRestApi.java
+++ templates/3.X.Y/scaffolding/osgi/service-module/src/test/java/.package/TestRestApi.java
@@ -1,38 +1,50 @@
 
 package <%-projectGroupId%>;
 
-import com.intuit.karate.Results;
-import com.intuit.karate.Runner;
-import org.junit.Assert;
-import org.junit.Test;
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.junit4.Karate;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.TestContainer;
 import org.ops4j.pax.exam.spi.PaxExamRuntime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import static org.awaitility.Awaitility.await;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * Generated with Water Generator.
  * Test class for BookOsgi Rest Services using karaf and karate.
  *
  */
+@RunWith(Karate.class)
+@KarateOptions(features = "classpath:karate")
 public class <%- projectSuffixUpperCase %>RestApiTest {
 
-    private Logger log = LoggerFactory.getLogger(<%- projectSuffixUpperCase %>RestApiTest.class);
-    @Test
-    public void test000_runKaraf() throws Exception {
+    private static final Logger log = LoggerFactory.getLogger(BookRestApiTest.class);
+    private static TestContainer testContainer;
+
+    @BeforeClass
+    public static void test000_runKaraf() throws Exception {
         //Running karaf externally
-        TestContainer testContainer = PaxExamRuntime.createContainer(<%- projectSuffixUpperCase %>TestConfiguration.class.getName());
-        Thread.sleep(5000);
-        //Running karate tests
-        Results results = Runner.path("classpath:karate").parallel(1);
-        if(results.getFailCount() > 0){
-            List<String> errors = results.getErrors();
-            errors.forEach(error -> log.error("\n ERROR: "+error));
-        }
-        Assert.assertEquals(0,results.getFailCount());
-        //Stopping karaf
+        testContainer = PaxExamRuntime.createContainer(BookTestConfiguration.class.getName());
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet request = new HttpGet("http://localhost:8080/water/status");
+        await().pollInterval(5,SECONDS).atMost(30, SECONDS).until(() -> {
+            log.info("Checking http services are online...");
+            HttpResponse response = httpClient.execute(request);
+            return response.getStatusLine().getStatusCode() >= 200 && response.getStatusLine().getStatusCode() < 300;
+        });
+    }
+
+    @AfterClass
+    public static void afterClass() {
         testContainer.stop();
     }
 }
