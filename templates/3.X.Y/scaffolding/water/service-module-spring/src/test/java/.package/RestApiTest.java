d86320b4096d08bc6d46df8fb66381ae86f6aa63
Aristide <aristide.cittadino@acsoftware.it>
2025-04-14 06:43:28 +0200

    fix on spring template

--- templates/3.X.Y/scaffolding/water/service-module-spring/src/test/java/.package/RestApiTest.java
+++ templates/3.X.Y/scaffolding/water/service-module-spring/src/test/java/.package/RestApiTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import <%-projectGroupId%>.service.<%-projectSuffixUpperCase%>Application;
 
 @SpringBootTest(classes = <%- projectSuffixUpperCase %>Application.class,
-        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {
         "water.rest.security.jwt.validate.by.jws=false",
         "water.rest.security.jwt.validate=false",
@@ -22,6 +22,9 @@ public class <%- projectSuffixUpperCase %>RestSpringApiTest {
     @Autowired
     private ComponentRegistry componentRegistry;
 
+    @LocalServerPort
+    private int serverPort;
+
     @BeforeEach
     void impersonateAdmin() {
         //jwt token service is disabled, we just inject admin user for bypassing permission system
@@ -31,6 +34,9 @@ public class <%- projectSuffixUpperCase %>RestSpringApiTest {
 
     @Karate.Test
     Karate restInterfaceTest() {
-        return Karate.run("../<%- projectSuffixUpperCase %>-service/src/test/resources/karate");
+        return Karate.run("../<%- projectSuffixUpperCase %>-service/src/test/resources/karate")
+                .systemProperty("webServerPort", String.valueOf(serverPort))
+                .systemProperty("host", "localhost")
+                .systemProperty("protocol", "http");
     }
 }
